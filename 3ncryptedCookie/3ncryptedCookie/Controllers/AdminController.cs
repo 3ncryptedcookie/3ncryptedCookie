9b192b5cec3503c2643127946fe8f217481a2d0e
EncryptedCookie <encrypted_cookie@outlook.com>
2019-03-26 08:47:42 +0400

    Added controllers

--- 3ncryptedCookie/3ncryptedCookie/Controllers/AdminController.cs
+++ 3ncryptedCookie/3ncryptedCookie/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using EncryptedCookie.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -9,22 +10,86 @@ namespace _3ncryptedCookie.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
-        {
-            List<Post> postList = new List<Post>
+        List<Post> postList = new List<Post>
             {
                 new Post{ Id = 1, Title = "Title1", Body = "Description", DateCreated = new System.DateTime(2015, 3, 10, 2, 15, 10), DateModified = new System.DateTime(2015, 3, 10, 2, 15, 10), DatePublished = new System.DateTime(2015, 3, 10, 2, 15, 10)},
-                new Post{ Id = 1, Title = "Title2", Body = "Description", DateCreated = new System.DateTime(2016, 3, 10, 2, 15, 10), DateModified = new System.DateTime(2016, 3, 10, 2, 15, 10), DatePublished = new System.DateTime(2016, 3, 10, 2, 15, 10)},
-                new Post{ Id = 1, Title = "Title3", Body = "Description", DateCreated = new System.DateTime(2017, 3, 10, 2, 15, 10), DateModified = new System.DateTime(2017, 3, 10, 2, 15, 10), DatePublished = new System.DateTime(2017, 3, 10, 2, 15, 10)},
+                new Post{ Id = 2, Title = "Title2", Body = "Description", DateCreated = new System.DateTime(2016, 3, 10, 2, 15, 10), DateModified = new System.DateTime(2016, 3, 10, 2, 15, 10), DatePublished = new System.DateTime(2016, 3, 10, 2, 15, 10)},
+                new Post{ Id = 3, Title = "Title3", Body = "Description", DateCreated = new System.DateTime(2017, 3, 10, 2, 15, 10), DateModified = new System.DateTime(2017, 3, 10, 2, 15, 10), DatePublished = new System.DateTime(2017, 3, 10, 2, 15, 10)},
             };
 
+        public IActionResult Index()
+        {
             return View(postList);
         }
 
+        //public IActionResult Index()
+        //{
 
-        //Link to create a new post
+        //    return RedirectToAction("Login", "Account");
+        //}
+
+
+        //GET: Admin/Create
         public ActionResult Create()
         {
+            return View();
+        }
+
+        //POST: Admin/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post post)
+        {
+            if(ModelState.IsValid)
+            {
+                postList.Add(post);
+            }
+
+            return View(post);
+        }
+
+        // GET: Admin/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    //Post post = postList.FindAll(id);
+        //    if (post == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(post);
+        //}
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Post restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(restaurant).State = EntityState.Modified;
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
+        //Link to create a new post
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        //Link to create a new post
+        public ActionResult View(int? id)
+        {
+            //Post post = postList.Find(id);
             return View();
         }
     }
--- 3ncryptedCookie/3ncryptedCookie/Controllers/HomeController.cs
+++ 3ncryptedCookie/3ncryptedCookie/Controllers/HomeController.cs
@@ -5,39 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using _3ncryptedCookie.Models;
+using EncryptedCookie.Models;
 
 namespace _3ncryptedCookie.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string searchTerm = null)
         {
-            return View();
-        }
+            List<Post> postList1 = new List<Post>
+            {
+                new Post{ Id = 1, Title = "Title1", Body = "Description", DateCreated = new System.DateTime(2015, 3, 10, 2, 15, 10), DateModified = new System.DateTime(2015, 3, 10, 2, 15, 10), DatePublished = new System.DateTime(2015, 3, 10, 2, 15, 10)},
+                new Post{ Id = 2, Title = "Title2", Body = "Description", DateCreated = new System.DateTime(2016, 3, 10, 2, 15, 10), DateModified = new System.DateTime(2016, 3, 10, 2, 15, 10), DatePublished = new System.DateTime(2016, 3, 10, 2, 15, 10)},
+                new Post{ Id = 3, Title = "Title3", Body = "Description", DateCreated = new System.DateTime(2017, 3, 10, 2, 15, 10), DateModified = new System.DateTime(2017, 3, 10, 2, 15, 10), DatePublished = new System.DateTime(2017, 3, 10, 2, 15, 10)},
+            };
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+            List<Post> model = postList1
+                                .OrderByDescending(r => r.DateCreated)
+                                .Where(r => searchTerm == null || r.Title.StartsWith(searchTerm))
+                                .ToList();
 
-            return View();
-        }
+            return View(model);
+         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+      }
 }
